45f806220654ccf5af77650b493a36544b370aa1
Emanuel Umbelino <emanuel.umbelino@hotmai.com>
2016-03-10 09:55:53 -0300

    Version Alpha 1.0

---
+++ Datas/Assets/Scripts/Gameplay/CastleBehaviour.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CastleBehaviour : MonoBehaviour {
+
+	public static bool lose;
+	public GameObject gameOver;
+    static float life;
+	private float atualnumber;
+	public Text score_t;
+	public Text record_t;
+	private float opa ;
+	private float record;
+	public GameObject newrecord;
+
+
+	void Start () {
+		atualnumber = 0;
+		lose = false;
+	}
+	void FixedUpdate () 
+	{
+		if (lose) {
+			SumToScore (EnemyBehaviour.destroyerTotal);
+			record = PlayerPrefs.GetFloat("Record");
+		}
+	}
+	void SumToScore(float myScore)
+	{
+		if (atualnumber < myScore && 0.2f <= Time.fixedTime - opa)
+		{
+			opa = Time.fixedTime;
+			atualnumber ++;
+		}
+		score_t.text = "Your Score:\n" + atualnumber.ToString ();
+		record_t.text = "Your Best:\n" + record.ToString();
+	}
+   
+	public void DamageMe(float damaged)
+	{
+		GameObject canvas = GameObject.Find("Canvas");
+		canvas.GetComponent<Canvas>().sortingOrder = 5;
+		gameOver.SetActive(true);
+		lose = true;
+		GetComponent<ScoreManaher> ().enabled = true;
+		if (PlayerPrefs.HasKey ("Record") && EnemyBehaviour.destroyerTotal > PlayerPrefs.GetFloat ("Record") || !PlayerPrefs.HasKey ("Record")) {
+			PlayerPrefs.SetFloat ("Record", EnemyBehaviour.destroyerTotal);
+			newrecord.SetActive (true);
+
+		}
+    }
+}
---
+++ Datas/Assets/Scripts/Gameplay/DirectionManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DirectionManager : MonoBehaviour {
+
+	private GameObject player;
+	public static bool onMousePress = false;
+	public static Vector2 worldPos;
+
+	void Start () 
+	{
+		onMousePress = false;
+		player = GameObject.Find("Player");
+	}
+
+	void LateUpdate () 
+	{
+		worldPos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		if(Input.GetButtonDown("Fire1"))
+		{
+			if(!CastleBehaviour.lose)
+			{
+				onMousePress = true;
+				if(Input.mousePosition.x >  Screen.width / 2)
+				{
+					player.transform.eulerAngles = new Vector3(0,0,0);
+				}
+				else
+				{
+					player.transform.eulerAngles = new Vector3(0,180,0);
+				}
+			}
+		}
+		if(Input.GetButtonUp("Fire1"))
+		{
+			onMousePress = false;
+		}
+	}
+}
